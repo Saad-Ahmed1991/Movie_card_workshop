a12a212a2b0ba61136a447d58c018645113bdfef
saad.ahmed1991 <ahmed.caparzo@gmail.com>
2022-11-08 10:19:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Add from "./components/Add/Add";
+import Filter from "./components/Filter/Filter";
+import Movielist from "./components/Movielist/Movielist";
+import { dataMovies } from "./data";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [newList, setnewList] = useState(dataMovies);
+  const [rate, setRate] = useState(0);
+
+  const addNewMovie = (newMovie) => {
+    setnewList([...newList, { ...newMovie, id: newList.length + 1 }]);
+  };
+  const deleteMovie = (iddelete) => {
+    setnewList(newList.filter((el) => el.id !== iddelete));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filter setInput={setInput} setRate={setRate} />
+      <Movielist
+        data={newList.filter(
+          (el) =>
+            el.title.toLowerCase().includes(input.toLocaleLowerCase().trim()) &&
+            el.rating >= rate
+        )}
+        deleteMovie={deleteMovie}
+      />
+      <Add addNewMovie={addNewMovie} />
     </div>
   );
 }
